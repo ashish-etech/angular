ecbbdd4a9bb85a8375d9218b4999ecbc023ff805
manish <manish@excellencetechnologies.in>
2017-03-10 13:15:37 +0530

    windowApp

--- windowRezise/windowApp.controller.js
+++ windowRezise/windowApp.controller.js
@@ -1,20 +1,20 @@
 var app = angular.module('windowApp', []);
-
 app.controller('windowAppController', function($scope) {
     
 });
-
 app.directive('resize', function(){
-    return function (scope){   
-        restrict : 'A';     
-        function resize (){
-            scope.height = window.innerHeight;
-            scope.width  = window.innerWidth;
-        };
-        window.onresize = function(){
+    return directive={   
+        restrict : 'A',     
+        link: function ($scope){
+            function resize(){
+                $scope.height=window.innerHeight;
+                $scope.width =window.innerWidth;
+            }                
+            window.onresize=function(){
+                resize();
+                $scope.$digest();
+            }
             resize();
-            scope.$digest();
-        };
-        resize();
-    }
+        }
+    }    
 }); 
