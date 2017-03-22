9b7c1350f2cecc81875ee60e85bd95dfe0f098c1
manish <manish@excellencetechnologies.in>
2017-03-22 15:11:35 +0530

    take poll

--- pollingApp/components/takePoll/takePollController.js
+++ pollingApp/components/takePoll/takePollController.js
@@ -2,44 +2,43 @@ var app = angular.module("angularForm");
 app.controller("takePollController", function($scope, getDataFactory,$timeout) {
     $scope.alerterror = false;
     $scope.alertSuccess = false;
-
-    $scope.record={}
+    $scope.record={};
 
     $scope.tableData = function() {
         url = '/list_polls';
         getDataFactory.getData(url).get().$promise
         .then(function(response) {
-        	console.log(response)
-        	if(response.error==0) {
-        		$scope.record = response.data;
-				console.log($scope.record);
-            }
-           
-        })
-    }
+            console.log(response);
+            if(response.error===0){
+                $scope.record = response.data;
+                console.log($scope.record);
+            }           
+        });
+    };
     $scope.tableData(); 
 
-    $scope.submitVote= function(option,data) {
+    $scope.submitVote= function(option,data){
         var newdata={"id":data._id,"option_text":option};
         url = '/do_vote';
-        console.log(newdata)
+        console.log(newdata);
         getDataFactory.getData(url).get(newdata).$promise
         .then(function(response) {
-             if (response.error==0) {               
-                console.log(response)
+             if (response.error===0) {               
+                console.log(response);
                 $scope.alertSuccess = true;
                 $scope.successMsg = option;
-                $timeout(function() {
+                $timeout(function(){
                     $scope.alertSuccess = false;
-                }, 3000)
-            }else{console.log(response)
+                }, 3000);
+            }
+            else{
+                console.log(response);
                 $scope.alerterror = true;
-                $scope.errMsg = response.data;
-                
+                $scope.errMsg = response.data;                
                 $timeout(function() {
                     $scope.alerterror = false;
-                }, 3000)
-            }
-        })
-    }  
-})
+                }, 3000);
+            };
+        });
+    };
+});
